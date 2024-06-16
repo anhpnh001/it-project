1be6dae544f9dd011c0b7b51f37e40f85c1fa3b8
MinhMan888 <minh.man0102002@gmail.com>
2024-06-16 19:12:24 +0700

    update

---
+++ src/components/CourseManager.tsx
@@ -0,0 +1,2 @@
+import CourseItem from '@/app/mycourses/page'; // Ensure this import path is correct
+import { Course } from '@/app/mycourses/interface/page';
--- src/components/LessonsContainer.tsx
+++ src/components/LessonsContainer.tsx
@@ -30,3 +30,6 @@ const LessonsContainer: React.FC = () => {
 }
 
 export default LessonsContainer
+
+
+
